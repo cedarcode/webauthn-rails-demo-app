beda64dede44bb7536bf17e04726aecf1fff7a99
Gonzalo <grzuy0@gmail.com>
2018-05-22 11:31:51 -0300

    basic app using webauthn gem

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,29 @@
 class SessionsController < ApplicationController
-  def new
 
+
+  def new
+  end
+
+  def create
+    credential_creation_options = WebAuthn.credential_creation_options
+
+    session[:challenge] = credential_creation_options[:challenge]
+
+    @credential_creation_options = credential_creation_options
+    respond_to :js
+  end
+
+  def callback
+    valid = WebAuthn.valid?(
+      original_challenge: session[:challenge],
+      attestation_object: params[:response][:attestationObject],
+      client_data_bin: params[:response][:clientDataJSON]
+    )
+
+    if valid
+      render json: { status: "ok" }, status: :ok
+    else
+      render json: { status: "forbidden"}, status: :forbidden
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :sessions, only: [:create]
+  post "callback", to: "sessions#callback"
   root to: "sessions#new"
 end
