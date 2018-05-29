e76cdd43d6629d0a6ac5de773384aa0e6acdec93
Gonzalo <grzuy0@gmail.com>
2018-05-29 09:47:06 -0300

    authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,34 +6,47 @@ class SessionsController < ApplicationController
     user = User.where(email: session_params[:email]).first_or_create!
 
     if user.credential.present?
-      warn "email already registerd"
+      credential_options = WebAuthn.credential_request_options
+
+    else
+      credential_options = WebAuthn.credential_creation_options
+      credential_options[:user][:id] = Base64.urlsafe_encode64(session_params[:email])
+      credential_options[:user][:name] = session_params[:email]
+      credential_options[:user][:displayName] = session_params[:email]
     end
 
-    credential_creation_options = WebAuthn.credential_creation_options
-    credential_creation_options[:user][:id] = Base64.urlsafe_encode64(session_params[:email])
-    credential_creation_options[:user][:name] = session_params[:email]
-    credential_creation_options[:user][:displayName] = session_params[:email]
-
-    user.update!(current_challenge: credential_creation_options[:challenge])
+    user.update!(current_challenge: credential_options[:challenge])
 
     session[:email] = session_params[:email]
 
     respond_to do |format|
-      format.json { render json: credential_creation_options }
+      format.json { render json: credential_options }
     end
   end
 
   def callback
-    attestation_response = WebAuthn::AuthenticatorAttestationResponse.new(
-      attestation_object: params[:response][:attestationObject],
-      client_data_json: params[:response][:clientDataJSON]
-    )
+    if params[:response][:attestationObject].present?
+      auth_response = WebAuthn::AuthenticatorAttestationResponse.new(
+        attestation_object: params[:response][:attestationObject],
+        client_data_json: params[:response][:clientDataJSON]
+      )
+    else
+      auth_response = WebAuthn::AuthenticatorAssertionResponse.new(
+        client_data_json: params[:response][:clientDataJSON],
+        authenticator_data: params[:response][:authenticatorData],
+        user_handle: params[:response][:userHandle],
+        signature: params[:response][:signature]
+      )
+    end
 
     user = User.where(email: session[:email]).take
 
     if user
-      if attestation_response.valid?(user.current_challenge, request.base_url)
-        user.update!(credential: Base64.encode64(attestation_response.credential_id))
+      if auth_response.valid?(user.current_challenge, request.base_url)
+        if params[:response][:attestationObject].present?
+          user.update!(credential: Base64.encode64(auth_response.credential_id))
+        end
+
         sign_in(user)
 
         render json: { status: "ok" }, status: :ok
