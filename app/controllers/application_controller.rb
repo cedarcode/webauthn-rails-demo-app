a13cd8e152a879fcbad8b40dc23926d93901e412
Gonzalo <grzuy0@gmail.com>
2018-05-29 13:29:33 -0300

    remove duplicated method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,10 +14,4 @@ class ApplicationController < ActionController::Base
         User.find_by(id: session[:user_id])
       end
   end
-
-  def enforce_current_user
-    if !current_user.present?
-      redirect_to new_session_path
-    end
-  end
 end
