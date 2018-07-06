a96c686d5cddfbbf8816c0ee603020ea11592f85
Gonzalo <grzuy0@gmail.com>
2018-07-06 10:14:50 -0300

    build: enable browser js error tracking with rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -2,7 +2,7 @@ Rollbar.configure do |config|
   # Without configuration, Rollbar is enabled in all environments.
   # To disable in specific environments, set config.enabled=false.
 
-  config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.access_token = ENV['ROLLBAR_POST_SERVER_ITEM_ACCESS_TOKEN']
 
   # Here we'll disable in 'test':
   if Rails.env.test? || Rails.env.development?
