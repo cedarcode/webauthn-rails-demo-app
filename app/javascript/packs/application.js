392214838500125e0c2bf1176ffcb9b882c35161
Gonzalo <grzuy0@gmail.com>
2018-10-13 00:00:14 -0300

    refactor: add stimulus with webpacker:install:stimulus

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,6 +15,13 @@ Turbolinks.start()
 import Rails from 'rails-ujs';
 Rails.start()
 
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("controllers", true, /.js$/)
+application.load(definitionsFromContext(context))
+
 import { MDCMenu, Corner as MDCMenu_Corner } from '@material/menu';
 import { MDCTextField } from '@material/textfield';
 
