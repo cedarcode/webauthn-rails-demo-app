38d353e67dd4fe001fa444706cbb355369ff8c58
Sander Groen <sandergroen27@gmail.com>
2018-10-21 22:25:53 +0200

    Removed unnecessary condition

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -35,15 +35,13 @@ class RegistrationsController < ApplicationController
     render json: { status: "forbidden" },
            status: :forbidden unless auth_response.valid?(str_to_bin(user.current_challenge), request.base_url)
 
-    if params[:response][:attestationObject].present?
-      credential = user.credentials.find_or_initialize_by(
-        external_id: Base64.strict_encode64(auth_response.credential.id)
-      )
-      credential.update!(
-        nickname: params[:credential_nickname],
-        public_key: Base64.strict_encode64(auth_response.credential.public_key)
-      )
-    end
+    credential = user.credentials.find_or_initialize_by(
+      external_id: Base64.strict_encode64(auth_response.credential.id)
+    )
+    credential.update!(
+      nickname: params[:credential_nickname],
+      public_key: Base64.strict_encode64(auth_response.credential.public_key)
+    )
 
     sign_in(user)
     render json: { status: "ok" }, status: :ok
