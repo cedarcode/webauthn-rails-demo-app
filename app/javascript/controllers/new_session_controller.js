915cbd03722a39208d9aab20f311fcb506ccdbdd
Gonzalo <grzuy0@gmail.com>
2018-10-27 17:38:24 -0300

    feat: make clear to the user when username it's not registered

--- app/javascript/controllers/new_session_controller.js
+++ app/javascript/controllers/new_session_controller.js
@@ -2,7 +2,11 @@ import { Controller } from "stimulus"
 import * as Credential from "credential";
 import * as Encoder from "encoder";
 
+import { MDCTextField } from '@material/textfield';
+
 export default class extends Controller {
+  static targets = ["usernameField"]
+
   create(event) {
     var [data, status, xhr] = event.detail;
     console.log(data);
@@ -12,5 +16,12 @@ export default class extends Controller {
       cred["id"] = Encoder.strToBin(cred["id"]);
     })
     Credential.get(credentialOptions);
+  }
+
+  error(event) {
+    let response = event.detail[0];
+    let usernameField = new MDCTextField(this.usernameFieldTarget);
+    usernameField.valid = false;
+    usernameField.helperTextContent = response["errors"][0];
   }
 }
---
+++ test/controllers/sessions_controller_test.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "should initiate registration successfully" do
+    User.create!(username: "alice")
+
+    post session_url, params: { session: { username: "alice" }, format: :json }
+
+    assert_response :success
+  end
+
+  test "should return error if creating session with inexisting username" do
+    post session_url, params: { session: { username: "alice" }, format: :json }
+
+    assert_response :unprocessable_entity
+    assert_equal ["Username doesn't exist"], JSON.parse(response.body)["errors"]
+  end
+end
