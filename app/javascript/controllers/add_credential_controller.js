536a1f859739c08cb7252a62e96fe9597435e0d4
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-01-22 17:11:32 -0300

    fix: fix 'ReferenceError: credential_nickname not defined' in Chrome 71

--- app/javascript/controllers/add_credential_controller.js
+++ app/javascript/controllers/add_credential_controller.js
@@ -9,7 +9,7 @@ export default class extends Controller {
 
     credentialOptions["challenge"] = Encoder.strToBin(credentialOptions["challenge"]);
     credentialOptions["user"]["id"] = Encoder.strToBin(credentialOptions["user"]["id"]);
-    credential_nickname = event.target.querySelector("input[name='credential[nickname]']").value;
+    var credential_nickname = event.target.querySelector("input[name='credential[nickname]']").value;
     var callback_url = `/credentials/callback?credential_nickname=${credential_nickname}`
 
     Credential.create(encodeURI(callback_url), credentialOptions);
