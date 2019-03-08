d0c818dd3866d54f3aeccb6a0db317122a3fffe2
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-03-08 19:12:23 -0300

    [WIP] Test sessions#callback

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require "webauthn/fake_authenticator"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
   test "should initiate registration successfully" do
@@ -23,5 +24,40 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :unprocessable_entity
     assert_equal ["Username doesn't exist"], JSON.parse(response.body)["errors"]
+  end
+
+  test "callback works" do
+    user = User.create!(username: "alice")
+
+    create_authenticator = WebAuthn::FakeAuthenticator::Create.new
+
+    user.credentials.create!(
+      nickname: "credential",
+      external_id: Base64.strict_encode64(create_authenticator.credential_id),
+      public_key: Base64.strict_encode64(create_authenticator.credential_key.public_key.to_bn.to_s(2))
+    )
+
+    post session_url, params: { session: { username: "alice" }, format: :json }
+
+    user.reload
+
+    authenticator = WebAuthn::FakeAuthenticator::Get.new(
+      challenge: Base64.strict_decode64(user.current_challenge),
+      rp_id: "www.example.com",
+      context: { origin: request.base_url }
+    )
+
+    post(
+      callback_session_url,
+      params: {
+        id: Base64.strict_encode64(authenticator.credential_id),
+        response: {
+          clientDataJSON: Base64.strict_encode64(authenticator.client_data_json),
+          signature: Base64.strict_encode64(authenticator.signature),
+          userHandle: "user",
+          authenticatorData: Base64.strict_encode64(authenticator.authenticator_data)
+        }
+      }
+    )
   end
 end
