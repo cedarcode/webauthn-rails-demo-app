e3fbb7e8d06c53e840a928b5c93542f25e04bff5
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-08-29 10:39:21 -0300

    refactor: update to latest webauthn credential options

--- test/system/add_credential_test.rb
+++ test/system/add_credential_test.rb
@@ -17,7 +17,7 @@ class AddCredentialTest < ApplicationSystemTestCase
     fill_in "registration_username", with: "User1"
     fill_in "Credential Nickname", with: "USB key"
 
-    WebAuthn::CredentialCreationOptions.stub_any_instance :raw_challenge, fixed_challenge do
+    WebAuthn::PublicKeyCredential::CreationOptions.stub_any_instance :raw_challenge, fixed_challenge do
       click_on "Register using WebAuthn"
       # wait for async response
       assert_button 'account_circle'
@@ -28,7 +28,7 @@ class AddCredentialTest < ApplicationSystemTestCase
 
     fill_in("credential_nickname", with: "Touch ID")
 
-    WebAuthn::CredentialCreationOptions.stub_any_instance :raw_challenge, fixed_challenge do
+    WebAuthn::PublicKeyCredential::CreationOptions.stub_any_instance :raw_challenge, fixed_challenge do
       click_on "Add Credential"
       # wait for async response
       assert_text 'Touch ID'
