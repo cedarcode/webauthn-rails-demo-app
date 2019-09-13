0c0c0e7a772906c3bd93e438ba7ebaa76f16609f
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-09-13 16:15:11 -0300

    refactor: use webauthn encoder instead of custom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,6 @@ class ApplicationController < ActionController::Base
   end
 
   def bin_to_str(bin)
-    Base64.urlsafe_encode64(bin, padding: false)
+    WebAuthn::Encoder.new(:base64url).encode(bin)
   end
 end
