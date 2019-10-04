8bd237f43db0f47f0ee7b70846ab12e6d02032bc
Gonzalo Rodriguez <grzuy0@gmail.com>
2019-10-04 16:42:21 -0300

    refactor: remove unecessary legacy conditional

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -21,17 +21,15 @@ class CredentialsController < ApplicationController
     webauthn_credential = WebAuthn::Credential.from_create(params)
 
     if webauthn_credential.verify(current_user.current_challenge)
-      if params[:response][:attestationObject].present?
-        credential = current_user.credentials.find_or_initialize_by(
-          external_id: Base64.strict_encode64(webauthn_credential.raw_id)
-        )
+      credential = current_user.credentials.find_or_initialize_by(
+        external_id: Base64.strict_encode64(webauthn_credential.raw_id)
+      )
 
-        credential.update!(
-          nickname: params[:credential_nickname],
-          public_key: webauthn_credential.public_key,
-          sign_count: webauthn_credential.sign_count
-        )
-      end
+      credential.update!(
+        nickname: params[:credential_nickname],
+        public_key: webauthn_credential.public_key,
+        sign_count: webauthn_credential.sign_count
+      )
 
       render json: { status: "ok" }, status: :ok
     else
