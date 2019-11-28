aad85869daf469859175b8deaeeba1126cca5416
Marcelo Casiraghi <marcelocasiraghi@gmail.com>
2019-11-28 17:57:12 -0300

    wip

--- app/models/credential.rb
+++ app/models/credential.rb
@@ -1,8 +1,5 @@
 # frozen_string_literal: true
 
-class Credential < ApplicationRecord
-  validates :external_id, :public_key, :nickname, :sign_count, presence: true
-  validates :external_id, uniqueness: true
-  validates :sign_count,
-            numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 2**32 - 1 }
+class Credential < WebAuthn::ActiveRecord::Credential
+  validates :nickname, presence: true
 end
