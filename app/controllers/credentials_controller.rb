88e27f6d096b5805801ea345157e55b35142fd5e
Gonzalo <grzuy0@gmail.com>
2020-05-16 15:20:57 -0300

    refactor: use WebAuthn::RelyingParty

--- app/controllers/credentials_controller.rb
+++ app/controllers/credentials_controller.rb
@@ -2,7 +2,7 @@
 
 class CredentialsController < ApplicationController
   def create
-    create_options = WebAuthn::Credential.options_for_create(
+    create_options = relying_party.options_for_registration(
       user: {
         id: current_user.webauthn_id,
         name: current_user.username,
@@ -19,29 +19,28 @@ class CredentialsController < ApplicationController
   end
 
   def callback
-    webauthn_credential = WebAuthn::Credential.from_create(params)
+    webauthn_credential = relying_party.verify_registration(
+      params,
+      current_user.current_challenge
+    )
 
-    begin
-      webauthn_credential.verify(session["current_registration"]["challenge"], user_verification: true)
+    credential = current_user.credentials.find_or_initialize_by(
+      external_id: Base64.strict_encode64(webauthn_credential.raw_id)
+    )
 
-      credential = current_user.credentials.find_or_initialize_by(
-        external_id: Base64.strict_encode64(webauthn_credential.raw_id)
-      )
-
-      if credential.update(
-        nickname: params[:credential_nickname],
-        public_key: webauthn_credential.public_key,
-        sign_count: webauthn_credential.sign_count
-      )
-        render json: { status: "ok" }, status: :ok
-      else
-        render json: "Couldn't add your Security Key", status: :unprocessable_entity
-      end
-    rescue WebAuthn::Error => e
-      render json: "Verification failed: #{e.message}", status: :unprocessable_entity
-    ensure
-      session.delete("current_registration")
+    if credential.update(
+      nickname: params[:credential_nickname],
+      public_key: webauthn_credential.public_key,
+      sign_count: webauthn_credential.sign_count
+    )
+      render json: { status: "ok" }, status: :ok
+    else
+      render json: "Couldn't add your Security Key", status: :unprocessable_entity
     end
+  rescue WebAuthn::Error => e
+    render json: "Verification failed: #{e.message}", status: :unprocessable_entity
+  ensure
+    session.delete("current_registration")
   end
 
   def destroy
