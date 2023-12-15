63a7e48e0a490cf069897e4b5de363a3bb97a90a
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2023-12-15 14:14:39 -0300

    build: migrate `messenger.js` to importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,4 +6,5 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javascripts/rails-ujs.esm.js"
 pin "credential"
+pin "messenger"
 pin "stub_credentials" if Rails.env.test?
