82aaaf3195d87ea989dfe257020a3b61e1252003
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2023-12-15 13:42:46 -0300

    build: pin `stub_credentials`

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,3 +4,4 @@ pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "stub_credentials", preload: true if Rails.env.test?
