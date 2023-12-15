c72aac0638b003a5947863d1a208727fdd69c046
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2023-12-15 15:20:45 -0300

    build: migrate `@material` components into importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,8 @@ pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javasc
 pin "credential"
 pin "messenger"
 pin "stub_credentials" if Rails.env.test?
+pin "@material/list", to: "https://ga.jspm.io/npm:@material/list@4.0.0/dist/mdc.list.js"
+pin "@material/menu", to: "https://ga.jspm.io/npm:@material/menu@4.0.0/dist/mdc.menu.js"
+pin "@material/snackbar", to: "https://ga.jspm.io/npm:@material/snackbar@4.0.0/dist/mdc.snackbar.js"
+pin "@material/textfield", to: "https://ga.jspm.io/npm:@material/textfield@4.0.0/dist/mdc.textfield.js"
+pin "@material/top-app-bar", to: "https://ga.jspm.io/npm:@material/top-app-bar@4.0.0/dist/mdc.topAppBar.js"
