2227d08f136ad0f0154350b08fcfcc7144edb270
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2023-12-15 14:14:21 -0300

    build: migrate `credential.js` to importmap

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,4 +5,5 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javascripts/rails-ujs.esm.js"
-pin "stub_credentials", preload: true if Rails.env.test?
+pin "credential"
+pin "stub_credentials" if Rails.env.test?
