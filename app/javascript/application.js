257f701fddf38487c18269986b52312aa71b5edc
Santiago Rodriguez <santiago.rodriguez@cedarcode.com>
2023-12-15 13:46:17 -0300

    build: migrate `@rails/ujs` to importmap

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,5 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers"
+import Rails from "@rails/ujs";
+
+Rails.start();
--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,5 @@ pin "application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "@rails/ujs", to: "https://ga.jspm.io/npm:@rails/ujs@7.1.2/app/assets/javascripts/rails-ujs.esm.js"
 pin "stub_credentials", preload: true if Rails.env.test?
