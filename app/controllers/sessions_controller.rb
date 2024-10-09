10e9812e9c0d1d75aa4a9e1c4cc3aed5d4f1e657
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2024-10-09 13:51:06 -0300

    Use global WebAuthn::Credential instead of custom WebAuthn::RelyingParty

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by(username: session_params[:username])
 
     if user
-      get_options = relying_party.options_for_authentication(
+      get_options = WebAuthn::Credential.options_for_get(
         allow: user.credentials.pluck(:external_id),
         user_verification: "required"
       )
@@ -26,19 +26,22 @@ class SessionsController < ApplicationController
   end
 
   def callback
+    webauthn_credential = WebAuthn::Credential.from_get(params)
+
     user = User.find_by(username: session[:current_authentication]["username"])
     raise "user #{session[:current_authentication]["username"]} never initiated sign up" unless user
 
-    begin
-      verified_webauthn_credential, stored_credential = relying_party.verify_authentication(
-        params,
-        session[:current_authentication]["challenge"],
-        user_verification: true,
-      ) do |webauthn_credential|
-        user.credentials.find_by(external_id: Base64.strict_encode64(webauthn_credential.raw_id))
-      end
+    credential = user.credentials.find_by(external_id: Base64.strict_encode64(webauthn_credential.raw_id))
 
-      stored_credential.update!(sign_count: verified_webauthn_credential.sign_count)
+    begin
+      webauthn_credential.verify(
+        session[:current_authentication]["challenge"],
+        public_key: credential.public_key,
+        sign_count: credential.sign_count,
+        user_verification: true,
+      )
+
+      credential.update!(sign_count: webauthn_credential.sign_count)
       sign_in(user)
 
       render json: { status: "ok" }, status: :ok
