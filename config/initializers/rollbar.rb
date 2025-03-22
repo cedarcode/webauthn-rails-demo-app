b5524827f9b554814b29ee2f94aa91b5dccacc2f
Emilio Cristalli <emilio.cristalli@cedarcode.com>
2025-03-22 20:50:16 -0300

    Don't report ActionController::RoutingError errors to Rollbar

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -35,6 +35,7 @@ Rollbar.configure do |config|
   #
   # You can also specify a callable, which will be called with the exception instance.
   # config.exception_level_filters.merge!('MyCriticalException' => lambda { |e| 'critical' })
+  config.exception_level_filters['ActionController::RoutingError'] = 'ignore'
 
   # Enable asynchronous reporting (uses girl_friday or Threading if girl_friday
   # is not installed)
