61158120ea7d2b34401c63dab0581362df6a08f8
rafaella-martino <rafaella.martino@cedarcode.com>
2025-08-15 11:43:03 -0300

    feat: merge js controllers into one

--- app/javascript/controllers/webauthn_credential_controller.js
+++ app/javascript/controllers/webauthn_credential_controller.js
@@ -30,4 +30,31 @@ export default class extends Controller {
       alert(error.message || error);
     }
   }
+
+  async get() {
+    const optionsResponse = await fetch(this.optionsUrlValue, {
+      method: "POST",
+      body: new FormData(this.element),
+      headers: {
+        "X-CSRF-Token": document.querySelector('meta[name="csrf-token"]')?.getAttribute("content")
+      }
+    });
+
+    try {
+      const data = await optionsResponse.json();
+      console.log(data);
+
+      if (optionsResponse.ok) {
+        console.log("Getting public key credential...");
+
+        const credential = await navigator.credentials.get({ publicKey: PublicKeyCredential.parseRequestOptionsFromJSON(data) })
+        this.hiddenCredentialInputTarget.value = JSON.stringify(credential);
+        this.element.submit();
+      } else {
+        alert(data.errors?.[0] || "Sorry, something wrong happened.");
+      }
+    } catch (error) {
+      alert(error.message || error);
+    }
+  }
 }
